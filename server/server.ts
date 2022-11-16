f37be2d6306409d315d76b7735d64ad66f564c47
Linimik <jameskimimk@gmail.com>
2022-11-16 10:43:16 -0500

    Update paths

--- server/server.ts
+++ server/server.ts
@@ -1,16 +1,17 @@
 import express from "express"
+import { resolve } from "path"
 
 const port = 8604
 const app = express()
 
-app.use(express.static("./static"))
-app.use("/assets", express.static("../assets"))
+app.use(express.static(resolve("./static")))
+app.use("/assets", express.static(resolve("../assets")))
 app.get("/game.wasm", (_, res) => {
-    res.sendFile("../target/wasm32-unknown-unknown/release/rogueman.wasm")
+    res.sendFile(resolve("../target/wasm32-unknown-unknown/release/rogueman.wasm"))
 })
 
 app.get("/", (_, res) => {
-    res.sendFile("./index.html")
+    res.sendFile(resolve("./index.html"))
 })
 
 app.listen(port, () => {
